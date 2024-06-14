dc396df0fc97d9f040b43569ac1c6a903c6ba153
Piotr Wasilewski <piotr.wasilewski@twoday.com>
2024-06-14 16:23:11 +0200

    Oiiii

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,20 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AddressFormComponent } from './address-form/address-form.component';
-import { DragDropComponent } from './drag-drop/drag-drop.component';
 import { GenericControlsComponent } from './generic-controls/generic-controls.component';
+import { FormGroupDirectiveComponent } from './form-group-directive/form-group-directive.component';
 
 const utilRoutes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: '', pathMatch: 'full', redirectTo: 'show-one' },
 ];
 
 export const navBarRoutes: Routes = [
+  {
+    path: 'show-one',
+    component: FormGroupDirectiveComponent,
+  },
   { path: 'home', component: HomeComponent },
   { path: 'address', component: AddressFormComponent },
-  { path: 'drag-drop', component: DragDropComponent },
   { path: 'generic-controls', component: GenericControlsComponent },
 ];
 
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,39 +1,29 @@
 import { Component, inject } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { AsyncPipe } from '@angular/common';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { RouterOutlet, Routes } from '@angular/router';
 import { navBarRoutes, routes } from '../app.routes';
+import { MaterialModule } from '../material-module';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss',
   standalone: true,
-  imports: [
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatListModule,
-    MatIconModule,
-    AsyncPipe,
-    RouterOutlet
-  ]
+  imports: [CommonModule, MaterialModule, RouterOutlet],
 })
 export class NavBarComponent {
   routes: Routes = routes;
   navBarRoutes: Routes = navBarRoutes;
   private breakpointObserver = inject(BreakpointObserver);
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
     .pipe(
-      map(result => result.matches),
+      map((result) => result.matches),
       shareReplay()
     );
 }
